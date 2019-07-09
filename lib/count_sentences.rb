01518466c566be00e17d207e57fc650d1c1d36fa
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-09 19:05:30 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+
+    self.end_with?(".")
+
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    split_with = [".","?","!"]
+    counter = 0
 
+    split_with.each do |splitter|
+      array << self.split(splitter)
+      counter +=
+      puts counter
+    end
+
+    # self.split(".","?","!")
   end
 end
