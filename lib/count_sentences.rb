a91f6a7bed5be156fa033e0a260a7e85b294ae75
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-09 19:20:44 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+
+    self.end_with?(".")
+
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    # split_with = [".","?","!"]
+    # counter = 0
+    # array = []
+    #
+    # split_with.each do |splitter|
+    #   # array << self.split(splitter)
+    #   self.split(/[!?.]+(?=\s|\z)/).count # + means one or more of these, ?= positive look ahead /z very end of a string
+    #   counter +=
+    #   puts counter
 
+        self.split(/[!?.]+(?=\s|\z)/).count # + means one or more of these, ?= positive look ahead /z very end of a string
+
+    # self.split(".","?","!")
   end
 end
